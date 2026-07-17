b996422512b9b09381f1c8b7b38f6f95e4051068
vaishn581 <vaishnavilangote80@gmail.com>
2026-07-17 17:00:54 +0530

    Add prediction module

--- backend/predict.py
+++ backend/predict.py
@@ -1,1 +1,13 @@
+import joblib
+import numpy as np
 
+# Load trained model
+model = joblib.load("../model/student_model.pkl")
+
+def predict_performance(study_hours, attendance, previous_marks):
+
+    data = np.array([[study_hours, attendance, previous_marks]])
+
+    prediction = model.predict(data)
+
+    return prediction[0]
